ee0c053023e174763a31cb459aae93f34d21fe9c
Ash <sgt16@yandex.ru>
2021-04-06 13:07:54 +0300

    0. Что такое обобщенные типы (generics) [#506116]

--- src/main/java/ru/job4j/iterator/FlatMap.java
+++ src/main/java/ru/job4j/iterator/FlatMap.java
@@ -7,45 +7,29 @@ import java.util.Collections;
 
 public class FlatMap<T> implements Iterator<T> {
     private final Iterator<Iterator<T>> data;
-    private Iterator<T> cursor;
+    private Iterator<T> cursor = Collections.emptyIterator();
+
 
     public FlatMap(Iterator<Iterator<T>> data) {
         this.data = data;
-        cursor = data.next();
     }
 
     @Override
-    public boolean hasNext() {
-        setupCursor();
-        return (cursor != null && cursor.hasNext());
+    public boolean  hasNext() {
+        return cursor != null;
     }
 
     @Override
     public T next() {
-        setupCursor();
-        if (cursor == null)
-        {
+        if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        return cursor.next();
-    }
+        T n = cursor.next();
 
-    private void setupCursor()
-    {
-        if (cursor != null && cursor.hasNext())
-        {
-            return;
+        while (cursor != null && !cursor.hasNext()) {
+            cursor = this.data.hasNext() ? this.data.next() : null;
         }
-        cursor = null;
-        while (data.hasNext())
-        {
-            Iterator<T> nextIterator = data.next();
-            if (nextIterator.hasNext())
-            {
-                cursor = nextIterator;
-                break;
-            }
-        }
+        return n;
     }
 
     public static void main(String[] args) {
