6293f061eb3fc90ebf83bb18ff20f508bef25073
Ash <sgt16@yandex.ru>
2021-04-16 16:45:36 +0300

    Users

---
+++ src/main/java/ru/job4j/collection/hash/User.java
@@ -0,0 +1,42 @@
+package ru.job4j.collection.hash;
+
+import java.util.*;
+
+public class User {
+    private String name;
+    private int children = 0;
+    private Calendar  birthday = null;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+   @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+   @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        Calendar now = Calendar.getInstance();
+        User first = new User("AAA", 3, now);
+        User second = new User("AAA", 3, now);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(first, new Object());
+        map.put(second, new Object());
+
+        map.entrySet().stream().forEach(System.out::println);
+
+    }
+
+}
