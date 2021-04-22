0337b4969c17fc5735e226c7040d8839b9657011
Ash <sgt16@yandex.ru>
2021-04-22 11:58:25 +0300

    0.1. FileOutputStream. [#506141]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            String rsl;
+            for (int i = 0; i < 99; i++) {
+                for (int j = 0; j < 99; j++) {
+                    rsl = i + " x " + j + " = " + (j * i) + "; ";
+                    out.write(rsl.getBytes());
+                }
+                out.write("\n".getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
