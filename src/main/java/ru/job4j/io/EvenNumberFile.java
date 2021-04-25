d5ae3a1597727361ca34ec1c68fbde3c2b5e335c
Ash <sgt16@yandex.ru>
2021-04-25 13:36:54 +0300

    0.1. FileOutputStream. [#506141]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -5,28 +5,25 @@ import java.io.FileOutputStream;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("even.txt")) {
-            try (FileOutputStream out = new FileOutputStream("evenOUT.txt")) {
+        try (FileInputStream in = new FileInputStream("even.txt");
+             FileOutputStream out = new FileOutputStream("evenOUT.txt")) {
                 StringBuilder builder = new StringBuilder();
                 int read;
                 while ((read = in.read()) != -1) {
                     builder.append((char) read);
                 }
                 String str = builder.toString();
-                String[] strLine = str.split("\r\n");
+                String[] strLine = str.split(System.lineSeparator());
                 for (var i : strLine) {
                      int digit = Integer.parseInt(i);
                     if (digit % 2 == 0) {
-                        out.write((digit + " even!\n").getBytes());
+                        out.write((digit + " even!" + System.lineSeparator()).getBytes());
                     } else {
-                        out.write((digit + " isnt even!\n").getBytes());
+                        out.write((digit + " isn't even!" + System.lineSeparator()).getBytes());
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
