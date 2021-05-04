94a3f4665a0e5d13dfb303457914384a73db6abc
Ash <sgt16@yandex.ru>
2021-05-04 16:55:49 +0300

    1. Бот [#506164]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,27 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 public class EchoServer {
+
+    private static String serverResponce(String str) {
+        String resp = "Непонятно что";
+            if (str.equals("Bye") || str.equals("Exit")) {
+            return "-1";
+        }
+        if (str.equals("Hello")) {
+            return "Hello";
+        }
+        if (str.equals("")) {
+            return "Пустой параметр";
+        }
+        if (str.equals("Any")) {
+            return "What.";
+        }
+        return resp;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,19 +32,24 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str = in.readLine();
+                    String answ;
                     while (!str.isEmpty()) {
                         if (str.contains("msg=")) {
                             int indexQ = str.indexOf("/?msg=");
                             int indexQend = str.indexOf(" HTTP");
                             String msg = str.substring(indexQ + 6, indexQend);
-                            if (msg.equals("Bye")) {
-                                System.out.println("Bye Bye");
+                            answ = serverResponce(msg);
+                            if (answ.equals("-1")) {
+                                out.write("Bye Bye".getBytes());
                                 server.close();
+                                break;
                             }
+                            out.write(answ.getBytes(Charset.forName("WINDOWS-1251")));
                         }
-                        System.out.println(str);
+                        //Не стал убирать цикл, может в будущем пригодится.
+                        break;
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    server.close();
                 }
             }
         }
