dfbff06c89104662d53ca4645bab5928e9f881fa
Ash <sgt16@yandex.ru>
2021-05-20 13:35:39 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#1000 #108543](Задача 1, Article)

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,32 +1,15 @@
 package ru.job4j.collection;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Article {
-    public static boolean generateBy(String text, String origin) {
-        String[] source = origin.split("");
-        String[] array = text.split("");
-        HashMap<String, Integer> counts = new HashMap<>();
-        for (var key : source) {
-            if (counts.containsKey(key)) {
-                counts.put(key, counts.get(key) + 1);
-            } else {
-                counts.put(key, 1);
-            }
-        }
-        for (var key : array) {
-            if (counts.containsKey(key)) {
-                int val = counts.get(key);
-                if (val > 0) {
-                    counts.put(key, val - 1);
-                }
-            }
-        }
-        for (int v : counts.values()) {
-            if (v != 0) {
-                return false;
-            }
-        }
-        return true;
+    public static boolean generateBy(String origin, String text) {
+       Set<String> source = new HashSet<>(Arrays.asList(origin.toLowerCase(Locale.ROOT).split("[^A-Za-zА-Яа-я0-9]")));
+       for (var v : text.toLowerCase(Locale.ROOT).split(" ")) {
+           if (source.add(v)) {
+               return false;
+           }
+       }
+       return true;
     }
 }
