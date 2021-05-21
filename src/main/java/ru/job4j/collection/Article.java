3f9009e6cee7b6618bec7b6ed6a64fab0e02e15d
Ash <sgt16@yandex.ru>
2021-05-21 13:05:45 +0300

    1. Экзамен. Структуры данных и алгоритмы. [#506088](Задача 1)

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -1,11 +1,13 @@
 package ru.job4j.collection;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Article {
     public static boolean generateBy(String origin, String text) {
-       Set<String> source = new HashSet<>(Arrays.asList(origin.toLowerCase(Locale.ROOT).split("[^A-Za-zА-Яа-я0-9]")));
-       for (var v : text.toLowerCase(Locale.ROOT).split(" ")) {
+        Pattern pattern = Pattern.compile("\\p{Punct}|\\p{Space}");
+       Set<String> source = new HashSet<>(Arrays.asList(origin.toLowerCase(Locale.ROOT).split(pattern.pattern())));
+       for (var v : text.toLowerCase(Locale.ROOT).split(pattern.pattern())) {
            if (source.add(v)) {
                return false;
            }
